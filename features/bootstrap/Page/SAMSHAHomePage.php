61ace0a4732907285f9a7204ea8c719427acf122
suswari <suswari91@gmail.com>
2018-06-07 10:50:23 -0400

    Test

--- features/bootstrap/Page/SAMSHAHomePage.php
+++ features/bootstrap/Page/SAMSHAHomePage.php
@@ -8,12 +8,28 @@
 
 namespace Page;
 
+use Behat\Mink\Session;
+use SensioLabs\Behat\PageObjectExtension\PageObject\Factory;
 use SensioLabs\Behat\PageObjectExtension\PageObject\Page;
+use WebDriver\Exception\Timeout;
 
 class SAMSHAHomePage extends CommonActions {
     protected $path = '/';
 
-    public $programsAndCampaingsLink = ".//a[@id='anch_22']";
+    public function __construct(Session $session, Factory $factory, array $parameters = array())
+    {
+        parent::__construct($session, $factory, $parameters);
+        try{
+            $session->getDriver()->setTimeouts(['page load'=>10000]);
+        }
+        catch (Timeout $exception){
+
+        }
+
+    }
+
+    public $programsAndCampaingsLink = ".//a[text()='Programs & Campaigns']";
+
 
 
     public function ClickProgramsAndCampaignsLink(){
